1bcb370ce2ba3d7430e0160aa009ce16223dae9b
JayceMXY <jayce_ma.xa@foxmail.com>
2018-04-13 14:57:51 +0800

    阻止事件冒泡

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-$(clickMe).on('click', function () {
+$(clickMe).on('click', function (e) {
     $(popover).show()
     setTimeout(function () {
         $(document).one('click', function () {
             $(popover).hide()
         })
     }, 0)
+})
+
+$(popover).on('click',function(e){
+    e.stopPropagation()
 })
